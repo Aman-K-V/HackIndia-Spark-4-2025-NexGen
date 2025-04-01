a520d884560ab9a10456806eed80f0dc028231ad
Aman Verma <amanverma1920coc@gmail.com>
2025-04-01 22:20:49 +0530

    Added initial project files

---
+++ drizzle.config.js
@@ -0,0 +1,13 @@
+const config = {
+    dialect: "postgresql",
+    schema: "./utils/db/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+      url: process.env.DATABASE_URL,
+      connectionString:
+        process.env.DATABASE_URL ,
+    },
+  };
+
+export default config;
+  
---
+++ src/app/metadata.tsx
@@ -0,0 +1,7 @@
+import { Metadata } from "next";    
+
+export const metadata: Metadata = {
+    title: "Ecoscan",
+    description: "Decentrailized Waste Management System",
+  };
+  
